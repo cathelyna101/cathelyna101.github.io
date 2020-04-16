28179b2ef8836490436ef3f47ab6c81c2200815b
cathelyna101 <60442545+cathelyna101@users.noreply.github.com>
2020-04-16 19:07:08 +0700

    added commit

---
+++ JQuery/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  console.log("page loaded");
+
+let titleClicked = false;
+$("#title").click(function () {
+  if(titleClicked == false)
+
+})
+
+  $("#title").click(function(){$("#title").html("How to Apologize To your Cat<em>Consistently</em>");})
+ {
+
+
+$("#what").mouseover(function(){
+  $("#catpic").fadeIn(5000)
+  $("body").css("background-color", "blue")
+})
+
+
+
+  $("#hide").click(function(){
+    $("p").hide();
+  });
+  $("#show").click(function(){
+    $("p").show();
+  });
+});
+})
+});
