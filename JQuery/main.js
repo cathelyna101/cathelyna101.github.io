5f6173893ef2f7abcf6ab20ed465b3ba8cb9ece1
cathelyna101 <60442545+cathelyna101@users.noreply.github.com>
2020-04-22 15:05:08 +0700

    commit
    updated

--- JQuery/main.js
+++ JQuery/main.js
@@ -1,22 +1,28 @@
 $(document).ready(function() {
   console.log("page loaded");
 
-let titleClicked = false;
-$("#title").click(function () {
-  if(titleClicked == false)
 
-})
-
-  $("#title").click(function(){$("#title").html("How to Apologize To your Cat<em>Consistently</em>");})
- {
+ //  $("#title").click(function(){$("#title").html("How to Apologize To your Cat<em>Consistently</em>");})
+ // {
+ $("#title").click(function(){
+   $("#consistenly").show();
+ });
 
 
 $("#what").mouseover(function(){
-  $("#catpic").fadeIn(5000)
-  $("body").css("background-color", "blue")
+  $("#catpic").fadeIn(500);
 })
 
+$("#what").mouseleave(function(){
+  $("#catpic").fadeOut(500);
+})
 
+$("#ones").mouseover(function(){
+  $("h3").css("font-size", "30pt");
+})
+
+$("#what").mouseleave(function(){
+  $("h3").css("font-size", "11pt");})
 
   $("#hide").click(function(){
     $("p").hide();
@@ -24,6 +30,4 @@ $("#what").mouseover(function(){
   $("#show").click(function(){
     $("p").show();
   });
-});
-})
 });
